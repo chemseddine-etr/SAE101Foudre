d1dfa2a6e37347f5e23f944e3d1dce0aa56a1845
ZachChll <Zacharie.Chaillou@etu.univ-smb.fr>
2025-12-14 16:41:00 +0100

    uc gameover + boutton quiter + boutton retour acceuil

---
+++ SAE101Foudre/GameOverUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAE101Foudre
+{
+    /// <summary>
+    /// Logique d'interaction pour GameOverUC.xaml
+    /// </summary>
+    public partial class GameOverUC : UserControl
+    {
+        public GameOverUC()
+        {
+            InitializeComponent();
+        }
+
+        private void butQuitterGameOver_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void butMenuGameOver_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).OuvrirUC(new MenuAccueil());
+        }
+    }
+}
